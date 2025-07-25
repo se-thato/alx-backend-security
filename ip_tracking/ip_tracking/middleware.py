1cf1a962129d7dfc5cd31e5694e72e8c84d78e5b
se-thato <thatoselepe80@gmail.com>
2025-07-25 19:20:45 +0200

    basic ip logging middleware

---
+++ ip_tracking/ip_tracking/middleware.py
@@ -0,0 +1,26 @@
+from .models import RequestLog
+
+class LogRequestMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        ip = self.get_client_ip(request)
+        path = request.path
+
+        # Save to database
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=path
+        )
+
+        response = self.get_response(request)
+        return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
